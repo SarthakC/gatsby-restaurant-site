2a48a60ef17b082417497e2b9c07a6fb1a870890
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-02-02 01:10:15 +0530

    added about, contact, menu pages

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/Global/Layout/Layout"
+import SEO from "../components/Global/SEO/SEO"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h3>Hello</h3>
+  </Layout>
+)
+
+export default AboutPage
---
+++ src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/Global/Layout/Layout"
+import SEO from "../components/Global/SEO/SEO"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h3>Hello</h3>
+  </Layout>
+)
+
+export default ContactPage
