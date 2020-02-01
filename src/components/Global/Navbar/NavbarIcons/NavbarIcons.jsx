63cb9367e5662179fa5628aacf0c23def488063c
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-02-02 01:00:23 +0530

    added navbar icons

--- src/components/Global/Navbar/NavbarIcons/NavbarIcons.jsx
+++ src/components/Global/Navbar/NavbarIcons/NavbarIcons.jsx
@@ -1,7 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
+import {
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+  IconWrapper,
+} from "./NavbarIcons.styles"
 
 const NavbarIcons = () => {
-  return <div>Nav icons</div>
+  const [state] = useState({
+    icons: [
+      {
+        id: 1,
+        icon: <FacebookIcon />,
+        path: "https://www.facebook.com/",
+      },
+      {
+        id: 2,
+        icon: <TwitterIcon />,
+        path: "https://www.twitter.com/",
+      },
+      {
+        id: 3,
+        icon: <InstagramIcon />,
+        path: "https://www.instagram.com/",
+      },
+    ],
+  })
+
+  const { icons } = state
+
+  return (
+    <IconWrapper>
+      {icons.map(({ path, icon, id }) => {
+        return (
+          <a href={path} key={id} target="_blank">
+            {icon}
+          </a>
+        )
+      })}
+    </IconWrapper>
+  )
 }
 
 export default NavbarIcons
---
+++ src/components/Global/Navbar/NavbarIcons/NavbarIcons.styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+import { colors, transFunction } from "../../../../utils/styles"
+
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
+
+export const IconWrapper = styled.div`
+  display: none;
+  a {
+    font-size: 1.3rem;
+  }
+  svg {
+    &:hover {
+      color: ${colors.mainYellow};
+      ${transFunction()}
+    }
+  }
+  @media (min-width: 768px) {
+    width: 10rem;
+    display: flex;
+    justify-content: space-around;
+  }
+`
+
+export const FacebookIcon = styled(FaFacebook)`
+  color: #3b579d;
+`
+export const TwitterIcon = styled(FaTwitter)`
+  color: #3ab7f0;
+`
+export const InstagramIcon = styled(FaInstagram)`
+  color: #da5f53;
+`
