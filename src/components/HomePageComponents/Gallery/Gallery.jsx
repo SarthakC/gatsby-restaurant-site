f63a066934275f0dc95505d8ef94b19154dfcea4
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-02-03 05:09:54 +0530

    refactor Gallery to use succint graphql query

--- src/components/HomePageComponents/Gallery/Gallery.jsx
+++ src/components/HomePageComponents/Gallery/Gallery.jsx
@@ -5,54 +5,35 @@ import Img from "gatsby-image"
 import { Section } from "../../Global"
 import { GalleryWrapper } from "./Gallery.styles"
 
-const query = graphql`
+export const query = graphql`
   {
-    img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
-      id
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    img2: file(relativePath: { eq: "homeGallery/img-2.jpeg" }) {
-      id
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    img3: file(relativePath: { eq: "homeGallery/img-3.jpeg" }) {
-      id
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
+    images: allFile(filter: { relativeDirectory: { eq: "homeGallery" } }) {
+      nodes {
+        id
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
         }
       }
     }
   }
 `
+const foodNames = ["awesome pizza", "tasty pork", "yummy steak"]
 
 const Gallery = () => {
   return (
     <StaticQuery
       query={query}
-      render={({ img1, img2, img3 }) => (
+      render={({ images: { nodes: images } }) => (
         <Section>
           <GalleryWrapper>
-            <div className="item item-1">
-              <Img fluid={img1.childImageSharp.fluid} />
-              <p>awesome pizza</p>
-            </div>
-            <div className="item item-2">
-              <Img fluid={img2.childImageSharp.fluid} />
-              <p>tasty pork</p>
-            </div>
-            <div className="item item-3">
-              <Img fluid={img3.childImageSharp.fluid} />
-              <p>yummy steak</p>
-            </div>
+            {images.map(({ id, childImageSharp }, index) => (
+              <div key={id}>
+                <Img fluid={childImageSharp.fluid} />
+                <p>{foodNames[index]}</p>
+              </div>
+            ))}
           </GalleryWrapper>
         </Section>
       )}
